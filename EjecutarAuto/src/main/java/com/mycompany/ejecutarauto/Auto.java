617490045ffc8e9d19c0e270cce5b9d1ac61719a
Steveen Cuenca <626tellor@gmail.com>
2023-05-03 20:17:04 -0500

    Ejemplo en clase

---
+++ EjecutarAuto/src/main/java/com/mycompany/ejecutarauto/Auto.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejecutarauto;
+
+public class Auto {
+    private String marca;
+    private Radio ra;
+
+    public Auto(String marca, Radio ra) {
+        this.marca = marca;
+        this.ra = ra;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Radio getRa() {
+        return ra;
+    }
+
+    public void setRa(Radio ra) {
+        this.ra = ra;
+    }
+
+    
+}
---
+++ EjecutarAuto/src/main/java/com/mycompany/ejecutarauto/Radio.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejecutarauto;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Radio {
+    private String modelo;
+    
+    public Radio(String mo){
+        this.modelo = mo;
+    }
+    
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+}
